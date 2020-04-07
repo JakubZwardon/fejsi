131773359f13572f68df8c65e041de66a264c313
jakub <jakubzwardon@gmail.com>
2020-04-07 11:18:37 +0200

    Add logo
    add top bar

--- includes/header.php
+++ includes/header.php
@@ -2,6 +2,7 @@
 
 require 'config/config.php';
 
+//Redirect not logged user
 if(isset($_SESSION['username'])) {
 	$userLoggedIn = $_SESSION['username'];
 } else {
@@ -12,6 +13,20 @@ if(isset($_SESSION['username'])) {
 
 <html>
 <head>
-	<title>fejsi- home</title>
+	<title>Welcome to Fejsi</title>
+
+	<!-- Javascript -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+	<script src="assets/js/bootstrap.js"></script>
+
+	<!-- CSS -->
+	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
 </head>
 <body>
+
+	<div class="top_bar">
+		<div class="logo">
+			<a href="index.php">Fejsi!</a>
+		</div>
+	</div>
