b64397413b9ca28ea127ab6db5ce7d32b6147269
jakub <jakubzwardon@gmail.com>
2020-04-07 16:03:15 +0200

    Add icons to toolbar

--- includes/header.php
+++ includes/header.php
@@ -3,8 +3,10 @@
 require 'config/config.php';
 
 //Redirect not logged user
-if(isset($_SESSION['username'])) {
+if (isset($_SESSION['username'])) {
 	$userLoggedIn = $_SESSION['username'];
+	$user_details_query = mysqli_query($con, "SELECT * FROM users WHERE  username='$userLoggedIn'");
+	$user = mysqli_fetch_array($user_details_query);
 } else {
 	header('Location: register.php');
 }
@@ -12,6 +14,7 @@ if(isset($_SESSION['username'])) {
 ?>
 
 <html>
+
 <head>
 	<title>Welcome to Fejsi</title>
 
@@ -20,13 +23,38 @@ if(isset($_SESSION['username'])) {
 	<script src="assets/js/bootstrap.js"></script>
 
 	<!-- CSS -->
+	<link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css">
 	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
 </head>
+
 <body>
 
 	<div class="top_bar">
+
 		<div class="logo">
 			<a href="index.php">Fejsi!</a>
 		</div>
+
+		<nav>
+			<a href="#">
+				<?php echo $user['first_name']; ?>
+			</a>
+			<a href="index.php">
+				<i class="fa fa-home fa-lg"></i>
+			</a>
+			<a href="#">
+				<i class="fa fa-envelope fa-lg"></i>
+			</a>
+			<a href="#">
+				<i class="fa fa-bell-o fa-lg"></i>
+			</a>
+			<a href="#">
+				<i class="fa fa-users fa-lg"></i>
+			</a>
+			<a href="#">
+				<i class="fa fa-cog fa-lg"></i>
+			</a>
+		</nav>
+
 	</div>
