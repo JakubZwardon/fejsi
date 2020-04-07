24a5632ea8d8bfd6412ea23d0b95dddcbd4a6537
jakub <jakubzwardon@gmail.com>
2020-04-07 22:44:30 +0200

    Add user details profile

--- index.php
+++ index.php
@@ -3,10 +3,25 @@ include("includes/header.php");
 ?>
 <div class="user_details column">
 	<a href=""><img src="<?php echo $user['profile_pic']; ?>"></a>
+
+	<div class="user_details_left_right">
+		<a href="#">
+			<?php
+			echo $user['first_name'] . " " . $user['last_name'];
+			?>
+		</a>
+		<br />
+
+		<?php
+		echo "Posts: " . $user['num_posts'] . "<br />";
+		echo "Likes: " . $user['num_likes'];
+		?>
+	</div>
+
 </div>
 
 <!-- closing div from header file -->
-</div>  
+</div>
 </body>
 
 </html>
