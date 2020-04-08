a1bf434b19f78dd59f932d73404d2f3ebb596c31
jakub <jakubzwardon@gmail.com>
2020-04-08 16:37:28 +0200

    Add user class

---
+++ includes/classes/User.php
@@ -0,0 +1,32 @@
+<?php
+
+class User {
+    private $user;
+    private $con;
+
+    public function __construct($con, $username) {
+        $this->con = $con;
+        $userDetailsQuery = mysqli_query($con, "SELECT * FROM users WHERE username='$username'");
+        $this->user = mysqli_fetch_array($userDetailsQuery);
+    }
+
+    public function getUserName() {
+        return $this->user['username'];
+    }
+
+    public function getNumPosts() {
+        $username = $this->user['username'];
+        $query = mysqli_query($this->con, "SELECT num_posts FROM users WHERE username='$username'");
+        $row = mysqli_fetch_array($query);
+        return $row['num_posts'];
+    }
+
+    public function getFirstAndLastName() {
+        $username = $this->user['username'];
+        $query = mysqli_query($this->con, "SELECT first_name, last_name FROM users WHERE username='$username'");
+        $row = mysqli_fetch_array($query);
+        return $row['first_name'] . " " . $row['last_name'];
+
+        //return $this->user['first_name'] . " " . $this->user['last_name'];
+    }
+}
