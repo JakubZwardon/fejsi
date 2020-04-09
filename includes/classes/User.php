4c9038ef857ce59da2f251ea40d9663818edc88a
jakub <jakubzwardon@gmail.com>
2020-04-09 14:58:21 +0200

    Check is account closed func

--- includes/classes/User.php
+++ includes/classes/User.php
@@ -29,4 +29,16 @@ class User {
 
         //return $this->user['first_name'] . " " . $this->user['last_name'];
     }
+
+    public function isClosed() {
+        $username = $this->user['username'];
+        $query = mysqli_query($this->con, "SELECT user_closed FROM users WHERE username='$username'");
+        $row = mysqli_fetch_array($query);
+
+        if($row['user_closed'] == "yes") {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
