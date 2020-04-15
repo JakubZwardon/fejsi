51d847970e89a4a484973baec7aca1a75aecfb4c
jakub <jakubzwardon@gmail.com>
2020-04-15 11:55:32 +0200

    Add handle for AJAX request

---
+++ includes/handlers/ajax_submit_profile_post.php
@@ -0,0 +1,12 @@
+<?php
+require '../../config/config.php';
+include("../classes/User.php");
+include("../classes/Post.php");
+
+//if is set something in post_modal then submit post
+if(isset($_POST['post_body'])) {
+    $post = new Post($con, $_POST['user_from']);
+    $post->submitPost($_POST['post_body'], $_POST['user_to']);
+}
+
+?>
