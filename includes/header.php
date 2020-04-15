a1362ff34c780497a736a243ed00e739f133a962
jakub <jakubzwardon@gmail.com>
2020-04-15 11:52:33 +0200

    Add fejsi.js file

--- includes/header.php
+++ includes/header.php
@@ -21,6 +21,7 @@ if (isset($_SESSION['username'])) {
 	<!-- Javascript -->
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 	<script src="assets/js/bootstrap.js"></script>
+	<script src="assets/js/fejsi.js"></script>
 
 	<!-- CSS -->
 	<link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
