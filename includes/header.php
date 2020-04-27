22cd1471d91e09fde955fc6255cb02afe6b59b9c
jakub <jakubzwardon@gmail.com>
2020-04-27 12:01:30 +0200

    Add infinitive scrolling for messages dropdown add class includes add unread messages badge

--- includes/header.php
+++ includes/header.php
@@ -1,6 +1,9 @@
 <?php
 
 require 'config/config.php';
+include("classes/User.php");
+include("classes/Post.php");
+include("classes/Message.php");
 
 //Redirect not logged user
 if (isset($_SESSION['username'])) {
@@ -42,14 +45,22 @@ if (isset($_SESSION['username'])) {
 		</div>
 
 		<nav>
+			<?php
+				$message = new Message($con, $userLoggedIn);
+				$numMessages = $message->getUnreadNumber();
+			?>
 			<a href="<?php echo $userLoggedIn ?>">
 				<?php echo $user['first_name']; ?>
 			</a>
 			<a href="index.php">
 				<i class="fa fa-home fa-lg"></i>
 			</a>
-			<a href="#">
+			<a href="javascript:void(0);" onclick="getDropdownData('<?php echo $userLoggedIn; ?>', 'message')">
 				<i class="fa fa-envelope fa-lg"></i>
+				<?php
+					if($numMessages > 0)
+						echo '<span class="notification_badge" id="unread_message">' . $numMessages . '</span>';
+				?>
 			</a>
 			<a href="#">
 				<i class="fa fa-bell-o fa-lg"></i>
@@ -65,7 +76,56 @@ if (isset($_SESSION['username'])) {
 			</a>
 		</nav>
 
+		<div class="dropdown_data_window" style="height: 0px; border: none;"></div>
+		<input type="hidden" id="dropdown_data_type" value="">
+
 	</div>
+
+	<script>
+
+	$(document).ready(function() {
+		debugger;
+
+		let userLoggedIn = '<?php echo $userLoggedIn?>';
+
+		$('.dropdown_data_window').scroll(function() {
+			debugger;
+			let innerHeight = $('.dropdown_data_window').innerHeight();
+			let scrollTop = $('.dropdown_data_window').scrollTop();
+			let page = $('.dropdown_data_window').find('.nextPageDropdownData').val();	//get the val og 'page' send from 'Post class- loadPostsFriends method'
+			let noMoreData = $('.dropdown_data_window').find('.noMoreDropdownData').val();	//get the val og 'noMorePosts' send from 'Post class- loadPostsFriends method'
+
+			if((scrollTop + innerHeight >= $('.dropdown_data_window')[0].scrollHeight) && (noMoreData == 'false')) {
+				let pageName;
+				let type = $('#dropdown_data_type').val();
+
+				if(type == 'notification') {
+					pageName = "ajax_load_notifications.php";
+				} else if(type == 'message') {
+					pageName = "ajax_load_messages.php";
+				}
+
+				let ajaxReq = $.ajax({
+					url: "includes/handlers/" + pageName,
+					type: "POST",
+					data: "page=" + page + "&user=" + userLoggedIn,
+					cache: false,
+
+					success: function(response) {
+						$('.dropdown_data_window').find('.nextPageDropdownData').remove();	//removes current next page
+						$('.dropdown_data_window').find('.noMoreDropdownData').remove();
+					
+						$('.dropdown_data_window').append(response);					
+					}
+				});
+			}	//end if
+
+			return false;
+		});		//end $(window).scroll(function()
+
+	});
+
+	</script>
 
 	<!-- this div will be closed in index file -->
 	<div class="wrapper">
